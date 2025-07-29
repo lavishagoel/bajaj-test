e44a3fefeb5d9d910944ec6f6c027d682791d04e
lavishagoel <lavishagoel7@gmail.com>
2025-07-29 21:04:53 +0530

    Initial commit

---
+++ src/test/java/com/example/bajaj_java_app/BajajJavaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bajaj_java_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BajajJavaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
